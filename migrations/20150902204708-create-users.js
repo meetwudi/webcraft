f027afcdbc9e7cfb19d61e65752f2134acf0f227
tjwudi <webmaster@leapoahead.com>
2015-09-04 21:24:32 -0700

    user id increment

--- migrations/20150902204708-create-users.js
+++ migrations/20150902204708-create-users.js
@@ -1,17 +1,17 @@
 var dbm = global.dbm || require('db-migrate')
 var type = dbm.dataType
-var tableNames = require('../app/db/table-structures')
+var databaseConfig = require('../app/config/database')
 
 console.log(dbm)
 
 exports.up = function (db, callback) {
-  db.createTable(tableNames.USERS_TABLE, {
-    id: { type: type.INTEGER, primaryKey: true },
+  db.createTable(databaseConfig.USERS_TABLE, {
+    id: { type: type.INTEGER, primaryKey: true, autoIncrement: true },
     username: type.STRING,
     password: type.STRING
   }, callback)
 }
 
 exports.down = function (db, callback) {
-  db.dropTable(tableNames.USERS_TABLE, callback)
+  db.dropTable(databaseConfig.USERS_TABLE, callback)
 }
